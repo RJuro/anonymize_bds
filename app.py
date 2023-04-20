b1d029354903936cc535432ce5287f8aac108e70
RJurow <roman.j@me.com>
2023-04-20 16:46:13 +0200

    -

--- app.py
+++ app.py
@@ -1,46 +1,55 @@
-import spacy
-from fastapi import FastAPI
-from pydantic import BaseModel
+# Import required modules
+import spacy  # For natural language processing
+from fastapi import FastAPI  # For creating the FastAPI application
+from pydantic import BaseModel  # For defining the input and output data models
 
+# Create a FastAPI application instance
 app = FastAPI()
+
+# Load the pre-trained language model
 model = spacy.load('api/models/en')
 
+# Define a Pydantic model to represent the incoming user request
 class UserRequestIn(BaseModel):
     text: str
 
+# Define a Pydantic model to represent the output data
 class EntitiesOut(BaseModel):
-    entities: list
-    anonymized_text: str
+    entities: list  # A list of entity objects containing information about each entity
+    anonymized_text: str  # The text with the identified entities anonymized
 
+# Define a route for the FastAPI application
 @app.post("/anonymize", response_model=EntitiesOut)
 def extract_entities(user_request: UserRequestIn):
+    # Get the text from the user request
     text = user_request.text
+    # Process the text using the pre-trained model
     doc = model(text)
 
+    # Extract the entities from the processed text and create a list of entity objects
     entities = [
         {
-            "start": ent.start_char,
-            "end": ent.end_char,
-            "type": ent.label_,
-            "text": ent.text,
+            "start": ent.start_char,  # Start position of the entity in the text
+            "end": ent.end_char,  # End position of the entity in the text
+            "type": ent.label_,  # Type of the entity (e.g., PERSON, ORG, etc.)
+            "text": ent.text,  # The text of the entity
         }
-        for ent in doc.ents
+        for ent in doc.ents  # Loop through each entity in the processed text
     ]
 
+    # Anonymize the identified entities in the text
     anonymized_text = list(text)
-
     for entity in entities:
         start = entity["start"]
         end = entity["end"]
         anonymized_text[start:end] = "X" * (end - start)
-
     anonymized_text = "".join(anonymized_text)
+
+    # Return the list of entities and the anonymized text as the response
     return {"entities": entities, "anonymized_text": anonymized_text}
 
 
-# add a simle get route to check if the server is running
+# Add a simple route to check if the server is running
 @app.get("/")
 def read_root():
     return {"Business Data Science 2023 🚀💪"}
-
-
--- dockerfile
+++ dockerfile
@@ -1,13 +1,23 @@
-FROM python:3.9
+# Use the official Python 3.9 image as the base image for the new Docker image
+FROM python:3.9 
 
+# Copy the contents of the current directory (the `.`) to the `/api` directory inside the Docker image
 COPY . /api
 
+# Install the Python packages listed in the `requirements.txt` file located in the `/api` directory
 RUN pip install -r /api/requirements.txt
 
+# Set the `PYTHONPATH` environment variable to the root directory inside the Docker container
 ENV PYTHONPATH=/
+
+# Set the working directory inside the Docker container to the root directory
 WORKDIR /
 
+# Expose port 8000 on the Docker container
 EXPOSE 8000
 
+# Specify that the entry point for the Docker container is the `uvicorn` command
 ENTRYPOINT ["uvicorn"]
+
+# Specify the default command to run when the container starts. This command starts the Uvicorn server, serving the `app` object in the `api.app` module, listening on all available network interfaces (`0.0.0.0`)
 CMD ["api.app:app", "--host", "0.0.0.0"]
